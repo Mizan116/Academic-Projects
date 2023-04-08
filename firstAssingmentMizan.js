bbc455a0c8f59fce9d6bdf4e9d57342ac17fc05e
Md Mizanur Rahman <86485553+Mizan116@users.noreply.github.com>
2023-04-08 12:36:52 +0600

    Add files via upload

---
+++ firstAssingmentMizan.js
@@ -0,0 +1,104 @@
+//Ques-1. variable :
+// Ans: Variable is the particular method in which method one can declare the Object, number string or any other unknown data. 
+//==========
+
+{
+    //Ques-2. How to write variable :
+// Ans: Variable can be write in this way:
+                    // variableType variableName = "Value";
+                    // Examples: 
+                    var num = 23;
+                    let string = 'abc';
+                    let bool = false;
+//==========
+}
+
+{
+    //Ques-3. How to write string type variable :
+//string type variable contains string type data.
+// Ans:                var variableName = "Value";(any string here.)
+                    // Examples: 
+                    var a = "23";
+                    let string = 'abc';
+                    let s = `ajks`;
+//==========
+
+}
+{
+    //Ques-4. How to write number type variable :
+//number type variable contains number type data.
+
+                    // variableType variableName = Value;(any number here.)
+                    // Examples: 
+                    var num = 23;
+                    let b = 1.324;
+                
+//==========
+}
+{
+    //Ques-5. How to write Boolean type variable :
+//boolean type variable contains that data that either false or true.
+                    // variableType variableName = True ;(False can be also here)
+                    // Examples: 
+                    var bool = false;
+                    let b = true;
+                
+//==========
+}
+{
+    //Ques-6. How to use ToupperCase and toLowerCase function :
+
+                    // Examples: 
+                    var a = "HI This IS MIZAN";
+                    console.log(a.toLowerCase());
+                    var b='hello Eshan Bro!';
+                    console.log(b.toUpperCase());
+                
+//==========
+}
+{
+    //Ques-7. Number of operators in JS :
+
+    // 5 operators total. they are:
+                    // 1. Addition(+) 2. substraction(-) 3. Multiplication(*) 4.Divison(/) 5. Modulus(%).
+                
+//==========
+}
+{
+    //Ques-8. uses of math.abs() :
+    var num = -344;
+    console.log(math.abs(num));//return 344
+    
+//==========
+}
+{
+    //Ques-9. uses of math.ceil() :
+    var num = 3.44;
+    console.log(math.ceil(num));//return 4
+    
+//==========
+}
+{
+    //Ques-10. uses of math.floor() :
+    var num = 3.44;
+    console.log(math.floor(num));//return 3
+    
+//==========
+}
+{
+    //Ques-11. uses of math.round() :
+    var num1 = 3.4;
+    console.log(math.round(num1));//return 3
+    var num2 = 3.5;
+    console.log(math.round(num2));//return 4
+    
+//==========
+}
+{
+    //Ques-12. uses of math.random() :
+   //It generates random numbers from 0 to 1.
+   
+    console.log(math.random());
+    
+//==========
+}
